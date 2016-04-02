1211a5e3238eb3ebad0d1d4c481504d30d2f35f4
Emanuel Dima <emanueldima@gmail.com>
2016-04-02 16:57:26 +0200

    copy input to output

--- main.go
+++ main.go
@@ -3,22 +3,63 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 var verbose bool
 
-func verb(v ...interface{}) {
-	if !verbose {
-		return
-	}
-	fmt.Println(v...)
-}
+var inputDir = "/mydata/input"
+var outputDir = "/mydata/output"
 
 func main() {
 	flag.BoolVar(&verbose, "v", true, "verbose output")
 	flag.Parse()
 
-	verb("Copying input to output")
+	print("Copying input to output")
+	inputFiles, err := ioutil.ReadDir(inputDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fi := range inputFiles {
+		src := filepath.Join(inputDir, fi.Name())
+		dst := filepath.Join(outputDir, fi.Name())
+		err := copyFile(src, dst)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
-	verb("Done")
+	print("Done")
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	cerr := out.Close()
+	if err != nil {
+		return err
+	}
+	return cerr
+}
+
+func print(v ...interface{}) {
+	if !verbose {
+		return
+	}
+	fmt.Println(v...)
 }
