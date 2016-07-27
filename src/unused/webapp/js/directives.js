c3c8ae83e20add1240ff6c583a1a490e257589b8
Wei Qiu <wei.qiu@uni-tuebingen.de>
2016-07-27 10:58:24 +0200

    Remove unused code

--- src/unused/webapp/js/directives.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gefdirectives = angular.module('gef.directives', []);
-
-gefdirectives.directive('appVersion', ['version', function(version) {
-		return function(scope, elm, attrs) {
-			elm.text(version);
-		};
-	}]);
--- src/unused/webapp/js/services.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-var gefservices = angular.module('gef.services', ['ngResource']);
-gefservices.value('version', 'v0.4.0');
