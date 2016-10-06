96a1ad032271bc91357cbf33fdbd645bfcc40212
Wei Qiu <wei.qiu@uni-tuebingen.de>
2016-10-06 12:08:07 +0200

    update config files

--- onchanged-gef-docker.sh
+++ onchanged-gef-docker.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 cd `dirname $0`
 
+GEF_DOCKER_DIR="/Users/wqiu/Projects/GEF/gef-docker"
+
 echo " --- replacing gef-docker in vm : " `date "+%H:%M:%S"`
 
 cd vagrant-docker-server
 vagrant ssh-config > .sshconfig
 
 ssh -F .sshconfig vagrant@default 'killall gef-docker'
-scp -F .sshconfig ../../gef-docker/gef-docker vagrant@default:/home/vagrant/
-scp -F .sshconfig ../../gef-docker/config.json vagrant@default:/home/vagrant/
+echo "killing old version of gef-docker"
+scp -F .sshconfig "$GEF_DOCKER_DIR/bin/linux_amd64/gef-docker" vagrant@default:/home/vagrant/
+scp -F .sshconfig "$GEF_DOCKER_DIR/config.json" vagrant@default:/home/vagrant/
 ssh -F .sshconfig vagrant@default '/home/vagrant/gef-docker' &
 echo " --- ... done"
 echo
--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 
-echo " --- Starting irods virtual machine"
-(cd vagrant-irods-server && vagrant up)
-if [ $? -ne 0 ]; then
-	echo "irods vm failed, exiting"
-	exit 1
-fi
-
 echo " --- Starting docker virtual machine"
 (cd vagrant-docker-server && vagrant up)
 if [ $? -ne 0 ]; then
@@ -19,5 +12,5 @@ fi
 
 if [ "$1" == "-w" ]; then
 	echo " --- Start watching ../gef-docker"
-	fswatch -0 -o ../gef-docker/gef-docker | xargs -0 -n 1 -I {} ./onchanged-gef-docker.sh
+	fswatch -0 -o ../gef-docker/bin/linux_amd64/gef-docker -docker | xargs -0 -n 1 -I {} ./onchanged-gef-docker.sh
 fi
