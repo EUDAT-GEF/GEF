f22be387cd3ca8660bba252b5c2446270f15f19a
Emanuel Dima <emanueldima@gmail.com>
2016-11-03 15:15:39 +0100

    fix go import paths, test fixes, misc

---
+++ backend-docker/gef-docker/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/eudat-gef/gef/backend-docker/dckr"
+	"github.com/eudat-gef/gef/backend-docker/server"
+)
+
+var configFilePath = "config.json"
+var config configuration
+
+type configuration struct {
+	Docker []dckr.Config
+	Server server.Config
+}
+
+func main() {
+	flag.StringVar(&configFilePath, "config", configFilePath, "configuration file")
+	flag.Parse()
+
+	var err error
+	config, err = readConfigFile(configFilePath)
+	if err != nil {
+		log.Fatal("FATAL while reading config files: ", err)
+	}
+	if len(config.Docker) == 0 {
+		log.Fatal("FATAL: empty docker configuration list:\n", config)
+	}
+
+	client, err := dckr.NewClientFirstOf(config.Docker)
+	if err != nil {
+		log.Print(err)
+		log.Fatal("Failed to make any docker client, exiting")
+	}
+
+	server := server.NewServer(config.Server, client)
+	log.Println("Starting GEF server at: ", config.Server.Address)
+	err = server.Start()
+	if err != nil {
+		log.Println("GEF server failed: ", err)
+	}
+}
+
+func readConfigFile(configfilepath string) (configuration, error) {
+	var config configuration
+	file, err := os.Open(configfilepath)
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	return config, err
+}
---
+++ backend-docker/server/httpmisc.go
@@ -0,0 +1,110 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"net/http"
+)
+
+// Response encapsulates a http ResponseWriter
+type Response struct {
+	http.ResponseWriter
+}
+
+func logRequest(r *http.Request) {
+	log.Println(r.Method, r.URL.Path)
+}
+
+func logParam(name, value string) {
+	log.Println("    ", name, "=", value)
+}
+
+// ServerError sets a 500/server error
+func (w Response) ServerError(message string, err error) {
+	str := fmt.Sprintf("    API Server ERROR: %s :: %s", message, err.Error())
+	log.Println(str)
+	http.Error(w, str, 500)
+}
+
+// ServerNewError sets a 500/server error
+func (w Response) ServerNewError(message string) {
+	str := fmt.Sprintf("    API Server ERROR: %s", message)
+	log.Println(str)
+	http.Error(w, str, 500)
+}
+
+// Location sets location header
+func (w Response) Location(loc string) Response {
+	w.Header().Set("Location", loc)
+	return w
+}
+
+// Ok sets 200/ok response code and body
+func (w Response) Ok(body interface{}) {
+	setCodeAndBody(w, 200, body)
+}
+
+// Created sets 201/created response code and body
+func (w Response) Created(body interface{}) {
+	setCodeAndBody(w, 201, body)
+}
+
+func setCodeAndBody(w Response, code int, body interface{}) {
+	var contentType string
+	var data []byte
+	var err error
+
+	if jsonMap, ok := body.(map[string]interface{}); ok {
+		data, err = json.Marshal(jsonMap)
+		if err != nil {
+			w.ServerError("json marshal: ", err)
+			http.Error(w, fmt.Sprintln("Server Error: unexpected Ok body type"), 500)
+			return
+		}
+		contentType = "application/json; charset=utf-8"
+	} else if str, ok := body.(string); ok {
+		contentType = "text/plain; charset=utf-8"
+		data = []byte(str)
+	} else {
+		log.Printf("ERROR: unexpected Ok body type: %T\n", body)
+		http.Error(w, fmt.Sprintln("Server Error: unexpected Ok body type"), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(code)
+	w.Write(data)
+	// log.Println("setCodeAndBody:", code, contentType, body)
+	log.Println("    HTTP", code, ",", contentType, ",", len(data), "bytes")
+}
+
+func jmap(kv ...interface{}) map[string]interface{} {
+	if len(kv) == 0 {
+		log.Println("ERROR: jsonmap: empty call")
+		return nil
+	} else if len(kv)%2 == 1 {
+		log.Println("ERROR: jsonmap: unbalanced call")
+		return nil
+	}
+	m := make(map[string]interface{})
+	k := ""
+	for _, kv := range kv {
+		if k == "" {
+			if skv, ok := kv.(string); !ok {
+				log.Println("ERROR: jsonmap: expected string key")
+				return m
+			} else if skv == "" {
+				log.Println("ERROR: jsonmap: string key is empty")
+				return m
+			} else {
+				k = skv
+			}
+		} else {
+			m[k] = kv
+			k = ""
+		}
+	}
+	return m
+}
