c115a45e0ed1e297a14a2a264dcb56c3a252cdf8
ubuntolog <kstchernov@gmail.com>
2016-11-04 15:59:20 +0100

    Removed debugging print statements

--- services/volume-inspector/main.go
+++ services/volume-inspector/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"github.com/eudat-gef/gef/services/volume-inspector/api"
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Server starting")
 	http.ListenAndServe(":8181", api.Handlers())
 }
