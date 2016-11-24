812434b4a5e6062ca68a50d45bb0095b99365c19
Alexandr Chernov <kstchernov@gmail.com>
2016-11-24 09:17:26 +0100

    Checking the list of volumes in tests

--- backend-docker/tests/apiserver_test.go
+++ backend-docker/tests/apiserver_test.go
@@ -2,13 +2,38 @@ package tests
 
 import (
 	"testing"
-	"github.com/EUDAT-GEF/GEF/backend-docker/dckr"
 	"github.com/EUDAT-GEF/GEF/backend-docker/config"
 	"github.com/EUDAT-GEF/GEF/backend-docker/server"
+	//"github.com/EUDAT-GEF/GEF/backend-docker/dckr"
 	"net/http/httptest"
-	"fmt"
 	"net/http"
+	"encoding/json"
+
+	"log"
+	"io/ioutil"
 )
+
+type VolList struct {
+	Volumes1 map[string]string
+}
+
+func getJson(callURL string, target interface{}) error {
+	request, err := http.NewRequest("GET", callURL, nil)
+	res, err := http.DefaultClient.Do(request)
+
+	//r, err := http.Get(callURL)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	return json.NewDecoder(res.Body).Decode(target)
+}
+
+func isJSON(s string) bool {
+	var js map[string]interface{}
+	return json.Unmarshal([]byte(s), &js) == nil
+}
 
 func TestServer(t *testing.T) {
 	settings, err := config.ReadConfigFile(configFilePath)
@@ -18,33 +43,88 @@ func TestServer(t *testing.T) {
 	clientConf = settings.Docker
 
 	c := newClient(t)
-	s := createServer(c, t, settings.Server)
+	s := server.NewServer(settings.Server, c)
+
 	srv := httptest.NewServer(s.Server.Handler)
-	lsUrl := fmt.Sprintf("%s/api/", srv.URL)
+	baseURL := srv.URL + "/api/"
+	ifAPIExist(baseURL, t)
+	callListVolumesHandler(baseURL + "volumes", t)
 
 
-	//reader := strings.NewReader(userJson)
-	request, err := http.NewRequest("GET", lsUrl, nil)
+
+
+
+
+}
+
+func ifAPIExist(callURL string, t *testing.T) bool {
+	request, err := http.NewRequest("GET", callURL, nil)
+	if err != nil {
+		t.Error(err)
+	}
 	res, err := http.DefaultClient.Do(request)
-
 	if err != nil {
 		t.Error(err)
 	}
 
 	if res.StatusCode != 200 {
-		t.Errorf("Success expected: %d", res.StatusCode)
+		t.Error("Error code: ", res.StatusCode)
+		t.Fail()
+		return false
+	} else {
+		return true
 	}
-
 }
 
-func createServer(client dckr.Client, t *testing.T, servConf server.Config) *server.Server {
-	/*config := server.Config{
-		Address: ":4142",
-		ReadTimeoutSecs: 10,
-		WriteTimeoutSecs: 10,
-	}*/
+func callListVolumesHandler(callURL string,  t *testing.T) {
+	request, err := http.NewRequest("GET", callURL, nil)
+	if err != nil {
+		t.Error(err)
+	}
 
-	server := server.NewServer(servConf, client)
+	res, err := http.DefaultClient.Do(request)
+	if err != nil {
+		t.Error(err)
+	}
 
-	return server
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		t.Error("Error code: ", res.StatusCode)
+		t.Fail()
+	} else {
+		htmlData, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Error("Cannot read response body: ", err)
+			t.Fail()
+		}
+		correctReply := isJSON(string(htmlData))
+
+		if correctReply != true {
+			t.Error("Reply is not JSON")
+			t.Fail()
+		}
+
+		c := make(map[string]interface{})
+		err = json.Unmarshal(htmlData, &c)
+		if err != nil {
+			t.Error("Error while reading JSON: ", err)
+			t.Fail()
+		}
+
+		// a string slice to hold the keys
+		k := make([]string, len(c))
+		i := 0
+		// copy c's keys into k
+		for s, _ := range c {
+			k[i] = s
+			i++
+		}
+		log.Println(k[0])
+
+		// the only found key should look like this
+		if k[0] != "Volumes" {
+			t.Error("Reply is not correct")
+			t.Fail()
+		}
+	}
 }
