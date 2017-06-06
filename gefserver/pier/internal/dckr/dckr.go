8aa747192e3e2235866ec45f578c87334ceee69d
Alexandr Chernov <kstchernov@gmail.com>
2017-06-06 14:46:56 +0200

    Added comments

--- gefserver/pier/internal/dckr/dckr.go
+++ gefserver/pier/internal/dckr/dckr.go
@@ -469,8 +469,10 @@ func (c Client) CreateSwarmService(repoTag string, cmdArgs []string, binds []Vol
 	}
 	if limits.CPUPeriod <= 0 {
 		return srv, &stdout, def.Err(nil, "CPUPeriod is not set in the config file")
-
 	}
+
+	/* Based on resources.CPUQuota = r.Limits.NanoCPUs * resources.CPUPeriod / 1e9
+	taken from https://github.com/moby/moby/blob/v1.12.0-rc4/daemon/cluster/executor/container/container.go#L331 */
 	calculatedNanoCPU := (limits.CPUQuota * 1e9) / limits.CPUPeriod
 
 	serviceCreateOpts := docker.CreateServiceOptions{
