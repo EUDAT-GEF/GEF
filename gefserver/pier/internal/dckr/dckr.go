25a54ad774f1e6428327f01d346f369203b73c31
Alexandr Chernov <kstchernov@gmail.com>
2017-06-12 15:28:28 +0200

    Added restart policy for swarm services

--- gefserver/pier/internal/dckr/dckr.go
+++ gefserver/pier/internal/dckr/dckr.go
@@ -474,11 +474,17 @@ func (c Client) CreateSwarmService(repoTag string, cmdArgs []string, binds []Vol
 
 	serviceCreateOpts := docker.CreateServiceOptions{
 		ServiceSpec: swarm.ServiceSpec{
+
 			TaskTemplate: swarm.TaskSpec{
 				ContainerSpec: swarm.ContainerSpec{
 					Image:   repoTag,
 					Mounts:  serviceMounts,
 					Command: cmdArgs,
+
+				},
+
+				RestartPolicy: &swarm.RestartPolicy{
+					Condition: "none",
 				},
 
 				Resources: &swarm.ResourceRequirements{
