a3283c725c01fa69940a2445c86b0a7d2f19d916
Alexandr Chernov <kstchernov@gmail.com>
2017-08-17 10:53:39 +0200

    Removed unused code

--- webui/src/components/Jobs.jsx
+++ webui/src/components/Jobs.jsx
@@ -8,9 +8,6 @@ import { bindActionCreators } from 'redux'
 import * as actions from '../actions/actions';
 import FileTree from './FileTree'
 
-const selectRowProp = {
-    mode: 'checkbox'
-};
 const inProgressColor = {
     color: '#f45d00'
 };
@@ -21,13 +18,7 @@ const successColor = {
     color: '#337ab7'
 };
 const progressAnimation = <img src="/images/progress-animation.gif" />;
-
-
-let allJobs = [];
 let jobStatusUpdateTimer;
-let activeJobs;
-let inactiveJobs;
-let failedJobs;
 
 class Jobs extends React.Component {
     constructor(props) {
@@ -144,61 +135,60 @@ class Jobs extends React.Component {
         var activeJobs = 0;
         var inactiveJobs = 0;
         var failedJobs = 0;
-        if (this.props.jobs) {
-            this.props.jobs.map((job) => {
-                let service = null;
-                for (var i = 0; i < this.props.services.length; ++i) {
-                    if (job.ServiceID == this.props.services[i].ID) {
-                        service = this.props.services[i];
+        this.props.jobs.map((job) => {
+            let service = null;
+            for (var i = 0; i < this.props.services.length; ++i) {
+                if (job.ServiceID == this.props.services[i].ID) {
+                    service = this.props.services[i];
+                    break;
+                }
+            }
+            let serviceName = (service && service.Name && service.Name.length) ? service.Name :
+                (service && service.ID && service.ID.length) ? service.ID : "unknown service";
+            let title = "Job from " + serviceName;
+
+            let execDuration = "";
+            if (job.State.Code == -1) {
+                let currentDate = new Date();
+                execDuration = currentDate - Date.parse(job.Created);
+                activeJobs += 1;
+            } else {
+                execDuration = Date.parse(job.Finished) - Date.parse(job.Created);
+                if (job.State.Code == 0) {
+                    inactiveJobs += 1;
+                } else {
+                    failedJobs += 1;
+                }
+            }
+
+            let ConsoleOutput = "";
+            if (job.Tasks) {
+                for (var t = 0; t < job.Tasks.length; ++t) {
+                    if (job.Tasks[t].Name == "Service execution") {
+                        ConsoleOutput = job.Tasks[t].ConsoleOutput;
                         break;
                     }
                 }
-                let serviceName = (service && service.Name && service.Name.length) ? service.Name :
-                    (service && service.ID && service.ID.length) ? service.ID : "unknown service";
-                let title = "Job from " + serviceName;
+            }
 
-                let execDuration = "";
-                if (job.State.Code == -1) {
-                    let currentDate = new Date();
-                    execDuration = currentDate - Date.parse(job.Created);
-                    activeJobs += 1;
-                } else {
-                    execDuration = Date.parse(job.Finished) - Date.parse(job.Created);
-                    if (job.State.Code == 0) {
-                        inactiveJobs += 1;
-                    } else {
-                        failedJobs += 1;
-                    }
+            let createdDate = new Date(job.Created);
+            let fmtCreatedDate = createdDate.toLocaleDateString('en-GB');
+            let fmtCreatedTime = createdDate.toLocaleTimeString('en-GB');
+
+            allJobs.push(
+                {
+                    "title": title, "id": job.ID,
+                    "created": fmtCreatedDate + " " + fmtCreatedTime,
+                    "duration": this.formatJobDuration(execDuration / 1000),
+                    "status": job.State.Status,
+                    "code": job.State.Code,
+                    "console": ConsoleOutput,
+                    "input": job.InputVolume,
+                    "output": job.OutputVolume
                 }
+            );
+        });
 
-                let ConsoleOutput = "";
-                if (job.Tasks) {
-                    for (var t = 0; t < job.Tasks.length; ++t) {
-                        if (job.Tasks[t].Name == "Service execution") {
-                            ConsoleOutput = job.Tasks[t].ConsoleOutput;
-                            break;
-                        }
-                    }
-                }
-
-                let createdDate = new Date(job.Created);
-                let fmtCreatedDate = createdDate.toLocaleDateString('en-GB');
-                let fmtCreatedTime = createdDate.toLocaleTimeString('en-GB');
-
-                allJobs.push(
-                    {
-                        "title": title, "id": job.ID,
-                        "created": fmtCreatedDate + " " + fmtCreatedTime,
-                        "duration": this.formatJobDuration(execDuration / 1000),
-                        "status": job.State.Status,
-                        "code": job.State.Code,
-                        "console": ConsoleOutput,
-                        "input": job.InputVolume,
-                        "output": job.OutputVolume
-                    }
-                );
-            });
-        }
         return [allJobs, activeJobs, inactiveJobs, failedJobs];
     }
 
